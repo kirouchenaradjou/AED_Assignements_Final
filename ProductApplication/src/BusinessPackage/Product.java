fa8313dff92ac26ca3fa84ba9aadc1f9aa5bc795
Raghavi <kirouchenaradjou.r@husky.neu.edu>
2017-09-13 12:30:27 -0500

    Lab Excercise : Product Application to create and view the details

---
+++ ProductApplication/src/BusinessPackage/Product.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessPackage;
+
+/**
+ *
+ * @author ragha
+ */
+public class Product {
+
+    private String productName;
+    private String productPrice;
+    private String productAvailability;
+    private String productDescription;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductAvailability() {
+        return productAvailability;
+    }
+
+    public void setProductAvailability(String productAvailability) {
+        this.productAvailability = productAvailability;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
